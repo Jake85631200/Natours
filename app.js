eeced6ea349a70cdc67f26b3bc48c1015cdddb5a
Jake85631200 <Jake85631200@gmail.com>
2024-09-27 19:02:06 +0800

    CSP changed

--- app.js
+++ app.js
@@ -60,14 +60,12 @@ app.use(
       connectSrc: [
         "'self'",
         "https://natours20240927-919f1ebf30df.herokuapp.com", // allowed url
-        // "https://js.stripe.com",
       ],
       scriptSrc: [
         "'self'", // 當前網域的腳本
         "https://js.stripe.com", // 允許來自 Stripe 的腳本
       ],
       frameSrc: ["'self'", "https://js.stripe.com"],
-      // imgSrc: ["'self'", "https://js.stripe.com"],
     },
   }),
 );

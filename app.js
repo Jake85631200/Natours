55feedea5212366f4368b926c2f7880110f8657f
Jake85631200 <Jake85631200@gmail.com>
2024-09-27 18:24:20 +0800

    changed helmet

--- app.js
+++ app.js
@@ -49,8 +49,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Set security HTTP headers
 // helmet: A collection of many smaller middleware that set HTTP headers.
-app.use(helmet());
-// app.use(helmet({ contentSecurityPolicy: false }));
+// app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 // app.use(
 //   helmet.contentSecurityPolicy({
